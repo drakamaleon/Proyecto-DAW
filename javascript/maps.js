179b178996964714d10a3b0e156b05c227728207
Carlos Leon <carlos.leon.moran@gmail.com>
2016-12-17 00:49:23 -0500

    Terminada estructura Ayudantias

--- javascript/maps.js
+++ javascript/maps.js
@@ -13,15 +13,37 @@ function initMap(lati,long) {
 
 
 $(document).ready(function(){
-	$(".btn.btn-link").click(function(e){
+  $('#myModal').on('shown.bs.modal', function () {
+        initializeMap();
+    });
+	/*$(".btn.btn-link").click(function(e){
 		edificio={}
 		edificio.nombre= $(e.target).html();
 		edificio.lat=-2.1481404;
 		edificio.long=-79.9666825;
 		initMap(edificio.lat,edificio.long);
-		$("#map").css({ height: "50vh"});
+		$("#map").css({ height: "50vh"});*/
 
 	});
-}	
+//}	
 
-);
+//)
+;
+
+
+function initializeMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(51.219987, 4.396237),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+          mapOptions);
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(51.219987, 4.396237)
+        });
+        marker.setMap(map);
+    }
+
+    //show map on modal
+    
