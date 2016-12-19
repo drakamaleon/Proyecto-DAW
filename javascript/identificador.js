029a904cdaf465ee3093a244b5d78b5c89880769
Carlos Leon <carlos.leon.moran@gmail.com>
2016-12-18 22:01:10 -0500

    Aumentado cambio de links para version inicial de las secciones

--- javascript/identificador.js
+++ javascript/identificador.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	console.log(identificador());
+	id=identificador();
+	console.log(id);
+	links(id);
+
 });
 
 
@@ -15,5 +18,39 @@ function identificador() {
 	}
 	else{
 		return "pub";
+	}
+}
+
+
+function links(tipo) {
+	 var param =""
+	if(tipo=="est"){
+		param= "?estudiante"
+	}
+
+	if(tipo=="prof"){
+		param ="?profesor"
+	}
+	var li = $(".row.nav.nav-tabs").children()
+	console.log(li)
+	for (var i = 0; i < li.length; i++) {
+		lin = $(li[i]).children()
+		a= lin[0]
+		
+		if($(a).html()=="Inicio"){
+				$(a).attr('href', 'index.html'+param);	
+		}
+		if($(a).html()=="El Curso"){
+			$(a).attr('href', 'curso.html'+param);	
+		}
+		if($(a).html()=="Semana a semana"){
+			$(a).attr('href', 'semana.html'+param);	
+		}
+		if($(a).html()=="Equipo"){
+			$(a).attr('href', 'index.html'+param);	
+		}
+		if($(a).html()=="Ayudantías"){
+			$(a).attr('href', 'ayudantia.html'+param);	
+		}
 	}
 }
