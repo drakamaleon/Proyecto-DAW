d309b15531bcc51ca0cca8caac7b5da82e3d5b87
Carlos Leon <carlos.leon.moran@gmail.com>
2016-12-19 02:51:25 -0500

    Terminada Enviar archivo Estudiante

--- javascript/sandP.js
+++ javascript/sandP.js
@@ -8,18 +8,225 @@ $(document).on('show','.accordion', function (e) {
         //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
     });
 
-var ej =[{"titulo":"TITULO","autor":"AUTOR","dificultad":"FACIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50"},
-		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"MEDIO","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50"},
-		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"FACIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50"},
-		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"DIFICIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50"},
-		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"FACIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50"}
+var ej =[{"titulo":"TITULO","autor":"AUTOR","dificultad":"FACIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50","descripcion":"des desde desde","entradas":"desc de entrads","salidas":"desc de salidas"},
+		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"MEDIO","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50","descripcion":"des desde desde","entradas":"desc de entrads","salidas":"desc de salidas"},
+		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"FACIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50","descripcion":"des desde desde","entradas":"desc de entrads","salidas":"desc de salidas"},
+		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"DIFICIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50","descripcion":"des desde desde","entradas":"desc de entrads","salidas":"desc de salidas"},
+		 {"titulo":"TITULO","autor":"AUTOR","dificultad":"FACIL","breve":"Resuen resumen","etiquetas":["et1","et2","et3"],"hecho":"50","descripcion":"des desde desde","entradas":"desc de entrads","salidas":"desc de salidas"}
 		]
 
 $(document).ready(function() {
 	if(identificador()=="est"){
 		mostrarEjercicios(ej);
+		resolverEjercicio(ej[0]);
+
 	}
+
+
 });
+
+function mensajeAceptado() {
+	// funcion para crear un modal de mensaje aceptado
+}
+
+
+
+
+function resolverEjercicio(ejercicio) {
+	modal = $(".resolver")
+
+		mod= document.createElement("div")
+			$(mod).addClass('modal fade')
+			$(mod).attr({
+				"id": 'myModal',
+				"role": 'dialog'
+			});
+
+			moDial=document.createElement("div")
+				$(moDial).addClass('modal-dialog')
+				cont=document.createElement("div")
+					$(cont).addClass('modal-content')
+					head=document.createElement("div")
+						$(head).addClass('modal-header')
+						but = document.createElement("button")
+							$(but).addClass('close')
+							$(but).attr({
+								"type": 'button',
+								"datadismiss": 'modal'
+							});
+							$(but).html("&times;")
+						tit = document.createElement("h4")
+							$(tit).addClass('modal-title')
+							$(tit).html(ejercicio.titulo)
+						$(head).append(but)
+						$(head).append(tit)
+					body=document.createElement("div")
+						$(body).addClass('modal-body')
+						res=document.createElement("div")
+							$(res).attr('id', 'resolucion');
+							$(res).css({
+								"width": 'auto',
+								"height": '85vh'
+							});
+
+							panelDesc= document.createElement("div")
+								$(panelDesc).addClass('panel panel-info')
+								heDesc=document.createElement("div")
+									$(heDesc).addClass('panel-heading')
+									titDesc=document.createElement("h3")
+										$(titDesc).html("Descripcion")
+										$(titDesc).addClass('panel-title')
+									$(heDesc).append(titDesc)
+
+								boDesc= document.createElement("div")
+									$(boDesc).addClass("panel-body")
+									$(boDesc).html(ejercicio.descripcion)	
+								$(panelDesc).append(heDesc)
+								$(panelDesc).append(boDesc)
+
+							panelIN= document.createElement("div")
+								$(panelIN).addClass('panel panel-info')
+								heIN=document.createElement("div")
+									$(heIN).addClass('panel-heading')
+									titIN=document.createElement("h3")
+										$(titIN).html("Entradas")
+										$(titIN).addClass('panel-title')
+									$(heIN).append(titIN)
+
+								boIN= document.createElement("div")
+									$(boIN).addClass("panel-body")
+									$(boIN).html(ejercicio.entradas)	
+								$(panelIN).append(heIN)
+								$(panelIN).append(boIN)
+
+							panelOUT= document.createElement("div")
+								$(panelOUT).addClass('panel panel-info')
+								heOUT=document.createElement("div")
+									$(heOUT).addClass('panel-heading')
+									titOUT=document.createElement("h3")
+										$(titOUT).html("Salidas")
+										$(titOUT).addClass('panel-title')
+									$(heOUT).append(titOUT)
+
+								boOUT= document.createElement("div")
+									$(boOUT).addClass("panel-body")
+									$(boOUT).html(ejercicio.salidas)	
+								$(panelOUT).append(heOUT)
+								$(panelOUT).append(boOUT)
+
+							panelFILE= document.createElement("div")
+								$(panelFILE).addClass('panel panel-info')
+								heFILE=document.createElement("div")
+									$(heFILE).addClass('panel-heading')
+									titFILE=document.createElement("h3")
+										$(titFILE).addClass('panel-title')
+										$(titFILE).html("Subir archivo")
+									$(heFILE).append(titFILE)
+
+								boFILE= document.createElement("div")
+									$(boFILE).addClass("panel-body")
+									formu=document.createElement("form")
+										url= document.location.href;
+										$(formu).attr({'action': "#","method":"get"});
+										lab=document.createElement("label")
+											$(lab).addClass('custom-file')
+											inp=document.createElement("input")
+												$(inp).addClass('custom-file-input')
+												$(inp).attr({
+													"lang": 'es',
+													"type": 'file',
+													"id": 'file'
+												});
+												spa=document.createElement("span")
+													$(spa).addClass('custom-file-control')
+												$(inp).append(spa)
+											$(lab).append(inp)
+										bot = document.createElement("a")
+											$(bot).addClass('btn btn-primary')
+											$(bot).html("Enviar")
+											$(bot).click(function(event) {
+												archivovalido(event)
+											});
+										$(formu).append(lab)
+										$(formu).append(bot)
+										$(formu).append(document.createElement("div"))
+									$(boFILE).append(formu)
+
+
+								$(panelFILE).append(heFILE)
+								$(panelFILE).append(boFILE)
+							$(res).append(panelDesc)
+							$(res).append(panelIN)
+							$(res).append(panelOUT)
+							$(res).append(panelFILE)
+						$(body).append(res)
+					$(cont).append(head)
+					$(cont).append(body)
+				$(moDial).append(cont)
+			$(mod).append(moDial)
+		$(modal).append(mod)
+}
+
+
+function archivovalido(evento) {
+	boton = evento.target
+	formula=$(boton).parent("form")
+	input = $(formula).find("input")[0]
+	mensaje=""
+	if($(input).val()==""){
+		mensaje = "COMPLETE Y SUBA EL ARCHIVO"
+
+	}
+	else {
+		mensaje = "ENVIO EXITOSO"
+	}
+	$($(formula).find("div")[0]).html(mensaje)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function mostrarEjercicios(ejercicios) {
 	contenido = $(".content")
